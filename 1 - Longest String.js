dd0682a032a024d0d89e588c1a3881fa78f2e7c2
JaySebagh <jaysebagh@gmail.com>
2019-04-27 14:30:30 -0700

    challenge 1

---
+++ 1 - Longest String.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of strings and return the longest string in the array.
+
+// For example:
+// const strings1 = ['short', 'really, really long!', 'medium'];
+// console.log(longestString(strings1)); // <--- 'really, really long!'
+
+function longestString(arr) {
+
+}
