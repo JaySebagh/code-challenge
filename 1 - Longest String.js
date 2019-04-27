8b9a2025c436bd59801dabb4c5462d7f14cb6920
JaySebagh <jaysebagh@gmail.com>
2019-04-27 14:31:03 -0700

    solved

--- 1 - Longest String.js
+++ 1 - Longest String.js
@@ -5,5 +5,24 @@
 // console.log(longestString(strings1)); // <--- 'really, really long!'
 
 function longestString(arr) {
-
-}
+    // Create empty array.
+    let lengthArr = [];
+    // For loop based off the length of the array given.
+    for( i = 0; i < arr.length; i++  ) {
+    // For each item in the array, count the length and push it to lengthArr.
+      lengthArr.push(arr[i].length);
+    }
+    // Find the biggest number in lengthArr.
+    let biggest = Math.max(...lengthArr);
+    // For loop based off the length of the array given.
+    for( i = 0; i < arr.length; i++  ) {
+    // If the length of the item in the array given is the same as the biggest number in lengthArr, return that item.
+      if(arr[i].length === biggest) {
+        return arr[i]
+      }
+    }
+  }
+  
+const answer = longestString(['short', 'really, really long!', 'medium'])
+  
+console.log(answer)
