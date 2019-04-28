335ff6d815aff1166fca0aaf11bc8772da57d0e1
JaySebagh <jaysebagh@gmail.com>
2019-04-28 13:39:21 -0700

    solved

--- 3 - Reverse Number.js
+++ 3 - Reverse Number.js
@@ -9,5 +9,15 @@
 // 555
 
 function reverseNumber(num) {
-
+    // convert the number to a string
+    let convert = num.toString()
+    // reverse the string using the same method used in Code Challenge 2 - Reverse String
+    const rev = convert.split("").reverse().join("")
+    // convert the string back into a number
+    convert = parseInt(rev)
+    return convert
 }
+  
+const answer = reverseNumber(12345);
+  
+console.log(answer)
