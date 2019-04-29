bdd8195e0eb7b12877a68a06e1474119354574ed
JaySebagh <jaysebagh@gmail.com>
2019-04-29 08:12:02 -0700

    challenge 4

---
+++ 4 - Reverse Case.js
@@ -0,0 +1,8 @@
+// Write a function that takes in a string, reverses the 'casing' of that string, and returns the "reversed-casing" string.
+
+// const string = 'HELLO world!';
+// console.log(reverseCase(string)); // <--- hello WORLD!
+
+function reverseCase(str) {
+
+}
