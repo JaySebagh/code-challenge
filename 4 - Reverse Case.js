e8c4b47ecd5929f2a02d1ac50f7f9de69d6970d6
JaySebagh <jaysebagh@gmail.com>
2019-04-29 08:22:47 -0700

    solved

--- 4 - Reverse Case.js
+++ 4 - Reverse Case.js
@@ -4,5 +4,22 @@
 // console.log(reverseCase(string)); // <--- hello WORLD!
 
 function reverseCase(str) {
-
+    // Let strCase be an empty string
+    let strCase = '';
+    // For loop based off the length of the string provided
+    for (let i = 0; i < str.length; i++) {
+    // check if the letter is uppercase
+      if (str[i] === str[i].toUpperCase()) {
+    // If the letter is uppercase, convert it to lowercase and push it to strCase.
+        strCase += str[i].toLowerCase();
+      } else {
+    // If the letter is lowercase, convert it to uppercase and push it to strCase.
+        strCase += str[i].toUpperCase();
+      }
+    }
+    return strCase;
 }
+  
+const answer = reverseCase("HELLO world!")
+  
+console.log(answer)
