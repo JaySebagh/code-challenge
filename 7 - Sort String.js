d0eca22809b6f114b19e032db706ba5ec8bdefde
JaySebagh <jaysebagh@gmail.com>
2019-05-02 07:46:51 -0700

    challenge 7

---
+++ 7 - Sort String.js
@@ -0,0 +1,15 @@
+// Write a function called sortString that takes a string of letters and returns a string with the letters sorted in alphabetical order.
+
+// Input:
+// 'dcba'
+// 'zycxbwa'
+// 'AzycxbCwBaA'
+
+// Expected Output:
+// 'abcd'
+// 'abcwxyz'
+// 'AABCabcwxyz'
+
+function sortString(str) {
+
+}
