a7cea3c726fb1354de5a6cb1474c12db4001b63d
JaySebagh <jaysebagh@gmail.com>
2019-05-07 20:46:24 -0700

    solved

--- JS 1.js
+++ JS 1.js
@@ -6,3 +6,13 @@
 // ambiguous now that I am a computer scientist? (Hint: yes).
 
 const unimaginativeArray = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+// empty array
+let nums = []
+// for loop based off unimaginativeArray length, += 3 to ge every 3rd value
+for(let i = 0; i < unimaginativeArray.length; i += 3) {
+// add number at the position to the nums array
+  nums = nums + unimaginativeArray[i];
+}
+
+console.log(nums)
