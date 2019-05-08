89bdd5e9ff6adb05830b325441e4291970a5fc71
JaySebagh <jaysebagh@gmail.com>
2019-05-07 20:45:03 -0700

    js 1

---
+++ JS 1.js
@@ -0,0 +1,8 @@
+// Write a "for" loop that console.log()'s the "zeroeth"
+// value in the unimaginitiveArray, and thereafter every
+// third value of the unimaginativeArray, i.e. 0, 3, 6,
+// and 9: the zeroth, third, sixth, and ninth values.
+// What is "zeroeth"? Has the word "first" become
+// ambiguous now that I am a computer scientist? (Hint: yes).
+
+const unimaginativeArray = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
