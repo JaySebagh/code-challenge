5c79f8dcb4cf7368016da3ec3eb5ffd303466c6e
JaySebagh <jaysebagh@gmail.com>
2019-05-11 15:36:02 -0700

    solved

--- Vowel Count.js
+++ Vowel Count.js
@@ -5,5 +5,21 @@
 // For the purposes of this exercise, we are not considering "y" as a vowel.
 
 function vowelCount(str) {
-
-}
+    // convert str to lowercase
+    let strCaps = str.toLowerCase()
+    // empty array
+    let vowels = []
+    // loop over the lowercase str based off length
+    for(let i = 0; i < strCaps.length; i++){
+    // check if it's a vowel
+      if(strCaps[i] === "a" || strCaps[i] === "e" || strCaps[i] === "i" || strCaps[i] === "o" || strCaps[i] === "u"){
+    // if it's a vowel, push it to the array
+        vowels += strCaps[i];
+      }
+    }
+    // return the length of the array
+    return vowels.length
+  }
+  
+  const answer = vowelCount('Hello world!');
+  console.log(answer);
