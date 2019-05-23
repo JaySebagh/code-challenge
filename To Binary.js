3973d04d9ae0ee851b0440b2abfc59ef6d714edd
JaySebagh <jaysebagh@gmail.com>
2019-05-23 16:30:37 -0700

    binary

--- To Binary.js
+++ To Binary.js
@@ -1,6 +1,9 @@
 // Given a positive (or 0) number, return a string of 1's and 0's representing it's binary value
 
 function toBinaryString(number){
-    
+    let convert = parseInt(number, 10).toString(2);
+    return convert
 }
 
+const answer = toBinaryString(16);
+console.log(answer);
