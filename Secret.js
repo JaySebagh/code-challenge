44fd5f091c1c215c901555bc193c4b4e33365034
JaySebagh <jaysebagh@gmail.com>
2019-06-14 16:47:40 -0700

    solved

--- Secret.js
+++ Secret.js
@@ -11,5 +11,12 @@
 // The secret society's name should be entirely uppercased.
 
 function societyName(friends) {
-
+  let secretFriends = []
+  for(let i = 0; i < friends.length; i++) {
+    secretFriends.push(friends[i].charAt(0).toUpperCase())
+  }
+  return secretFriends.sort().join("")
 }
+
+const answer = societyName(["Adam", "Sarah", "Malcolm"]);
+console.log(answer);
