d8bb65a822823c98d046786c32fee9c7119ea986
JaySebagh <jaysebagh@gmail.com>
2019-06-15 13:53:30 -0700

    power

---
+++ Power.js
@@ -0,0 +1,11 @@
+// Write a function that returns true if k^k == n for input (n, k).
+
+// Examples
+// kToK(4, 2) ➞ true
+// kToK(387420489, 9) ➞ true # 9^9 == 387420489
+// kToK(3124, 5) ➞ false
+// kToK(17, 3) ➞ false
+
+function kToK(n, k) {
+
+}
