cc3acfe3e1ce8c98e0b3b3d606641507cced4b78
JaySebagh <jaysebagh@gmail.com>
2019-06-18 15:06:43 -0700

    countdown

---
+++ Countdown.js
@@ -0,0 +1,13 @@
+// Take in a number and return an array counting down to zero.
+
+// Example
+// countdown(3) ➞ [3, 2, 1, 0]
+// countdown(5) ➞ [5, 4, 3, 2, 1, 0]
+// countdown(10) ➞ [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+function countdown(start) {
+
+}
+
+const answer = countdown(3);
+console.log(answer);
