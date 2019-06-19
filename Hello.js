c1b75e5b68bc715b0b7bf3ddec710b76fbad6feb
JaySebagh <jaysebagh@gmail.com>
2019-06-19 13:58:41 -0700

    solved

--- Hello.js
+++ Hello.js
@@ -5,7 +5,15 @@
 // greetPeople(["Olan", "Chris", "Jay"]) ➞ "Hello Olan, Hello Chris, Hello Jay"
 
 function greetPeople(names) {
-
+  // create empty array
+  let namesArray = [];
+  // for loop based of the length of the given array of names
+  for(let i = 0; i < names.length; i++){
+  // push each name into the new array using string interpolation
+    namesArray.push(`Hello ${names[i]}`)
+  }
+  // return the array as a concat string
+  return namesArray.join(", ");
 }
 
 const answer = greetPeople(["Olan", "Chris", "Jay"]);
