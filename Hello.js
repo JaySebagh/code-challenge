af030b1f0ca3cbaf798bb56e3e8c6256cfb7ce72
JaySebagh <jaysebagh@gmail.com>
2019-06-19 13:57:46 -0700

    hello

---
+++ Hello.js
@@ -0,0 +1,12 @@
+//  Take an array of names and return the greeting "Hello", comma seperated.
+
+// greetPeople(["Jay"]) ➞ "Hello Jay"
+// greetPeople(["Chris", "Jay"]) ➞ "Hello Chris, Hello Jay"
+// greetPeople(["Olan", "Chris", "Jay"]) ➞ "Hello Olan, Hello Chris, Hello Jay"
+
+function greetPeople(names) {
+
+}
+
+const answer = greetPeople(["Olan", "Chris", "Jay"]);
+console.log(answer);
