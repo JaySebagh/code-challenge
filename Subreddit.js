0381ed9b630e89cbb2e5ab25f47e6da8239d2206
JaySebagh <jaysebagh@gmail.com>
2019-06-20 16:51:51 -0700

    subreddit

---
+++ Subreddit.js
@@ -0,0 +1,13 @@
+// Create a function that returns the subreddit.
+
+// Examples
+// subReddit("https://www.reddit.com/r/osugame/") ➞ "osugame"
+// subReddit("https://www.reddit.com/r/javascript/") ➞ "javascript"
+// subReddit("https://www.reddit.com/r/reactjs/") ➞ "reactjs"
+
+function subReddit(link) {
+
+}
+
+const answer = subReddit("https://www.reddit.com/r/programmerhumor/");
+console.log(answer);
