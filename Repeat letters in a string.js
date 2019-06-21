a2f46ee2565d83ad0e6f8a4b4d74263155d3b887
JaySebagh <jaysebagh@gmail.com>
2019-06-21 16:24:03 -0700

    repeat

---
+++ Repeat letters in a string.js
@@ -0,0 +1,8 @@
+// Write a function that returns a string with each letter repeated 'n' times.
+
+// Example:
+// repeat("string", 6) --> "ssssssttttttrrrrrriiiiiinnnnnngggggg"
+
+function repeats(str, n) {
+
+}
