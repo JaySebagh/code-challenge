c01ff00e4effa82415fff9de37481439a453a2b5
JaySebagh <jaysebagh@gmail.com>
2019-06-21 16:24:23 -0700

    solved

--- Repeat letters in a string.js
+++ Repeat letters in a string.js
@@ -4,5 +4,8 @@
 // repeat("string", 6) --> "ssssssttttttrrrrrriiiiiinnnnnngggggg"
 
 function repeats(str, n) {
-
+    return str.split("").map((letter) => letter.repeat(n)).join("")
 }
+
+const answer = repeats("string", 6)
+console.log(answer)
