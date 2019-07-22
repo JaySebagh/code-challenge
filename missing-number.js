165305a21577f00bf88775419512a5c7548513cf
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-07-22 16:11:19 -0700

    Create missing-number.js

---
+++ missing-number.js
@@ -0,0 +1,9 @@
+// Return the missing number in an array of numbers between 1 and 10
+
+// Example:
+// missingNum([1, 2, 3, 4, 6, 7, 8, 9, 10]) --> 5
+// missingNum([7, 2, 3, 6, 5, 9, 1, 4, 8]) --> 10
+
+function missingNum(arr) {
+
+}
