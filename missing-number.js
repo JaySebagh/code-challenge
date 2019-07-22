9500a4a2be3e132b700b7b6fafa5324f9ecfc2b3
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-07-22 16:11:46 -0700

    solved

--- missing-number.js
+++ missing-number.js
@@ -5,5 +5,16 @@
 // missingNum([7, 2, 3, 6, 5, 9, 1, 4, 8]) --> 10
 
 function missingNum(arr) {
-
+    arr = arr.sort(function(a,b){
+    return a-b;
+  });
+  const completeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for(let i = 0; i < completeArr.length; i++){
+    if(completeArr[i] !== arr[i]){
+      return completeArr[i]
+    }
+  }
 }
+
+const answer = missingNum([1, 2, 3, 4, 6, 7, 8, 9, 10]);
+console.log(answer)
