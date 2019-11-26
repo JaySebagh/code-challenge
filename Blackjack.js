c060358d38d6ca15d724a81fb91799d44a8accbe
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-11-26 15:28:33 -0800

    Create Blackjack.js

---
+++ Blackjack.js
@@ -0,0 +1,11 @@
+// In BlackJack, cards are counted with -1, 0, 1 values:
+
+//     2, 3, 4, 5, 6 are counted as +1
+//     7, 8, 9 are counted as 0
+//     10, J, Q, K, A are counted as -1
+
+// Create a function that counts the number and returns it from the array of cards provided.
+
+function count(deck){
+
+}
