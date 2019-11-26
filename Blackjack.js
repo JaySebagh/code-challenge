fb087e8e7ba5cbe0ca947ef16000882334b8db15
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-11-26 15:29:01 -0800

    Update Blackjack.js

--- Blackjack.js
+++ Blackjack.js
@@ -6,6 +6,20 @@
 
 // Create a function that counts the number and returns it from the array of cards provided.
 
-function count(deck){
-
+function count(deck) {
+  let addOne = [2, 3, 4, 5, 6]
+  let minusOne = [10, "J", "Q", "K", "A"]
+  let count = 0
+  for(let i = 0; i < deck.length; i++){
+    if(addOne.includes(deck[i])){
+      count += 1
+    }
+    else if(minusOne.includes(deck[i])){
+      count -=1
+    }
+  }
+  return count
 }
+
+const answer = count([5, 9, 10, 3, "J", "A", 4, 8, 5])
+console.log(answer)
