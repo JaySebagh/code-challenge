1a60784827bbb9179602dfd41602f4ad50b1bb0b
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-12-08 16:01:52 -0800

    Create LC-LongestCommonPrefix.js

---
+++ LC-LongestCommonPrefix.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+// Input: ["flower","flow","flight"]
+// Output: "fl"
+
+var longestCommonPrefix = function(strs) {
+}
