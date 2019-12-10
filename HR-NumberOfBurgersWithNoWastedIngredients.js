2238fbda7206fc4d025140abd23a8c2da5c5ae26
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-12-10 14:30:36 -0800

    Update HR-NumberOfBurgersWithNoWastedIngredients.js

--- HR-NumberOfBurgersWithNoWastedIngredients.js
+++ HR-NumberOfBurgersWithNoWastedIngredients.js
@@ -30,5 +30,57 @@
 // Output: [0,1]
 
 var numOfBurgers = function(tomatoSlices, cheeseSlices) {
-
+    // if there are no ingredients, return [0, 0]
+    // if there aren't at least 2 tomato slices, return []
+    // check if the tomatoes are divisable by 2
+        // if it's not, return []
+    
+    // no matter what, each burger requires 1 slice of cheese
+    // at minimum, each burger requires 2 slices of tomatoes
+    // check if the tomatoes are at least double the amount of cheese slices
+        // if it's not, return []
+    
+    // distribute all the tomatos (by 2) until there are no more cheese slices
+    // distriburte an additional 2 slices until none are left
+    
+    
+  
+    // array to hold answer
+    totalBurgers = []
+    // variables to hold number of small and jumbo burgers
+    let jumboBurgers = 0
+    let smallBurgers = 0
+    
+    // making sure we have ingredients
+    if((tomatoSlices && cheeseSlices) === 0){
+        return [0, 0]
+    }
+    
+    // checking if we have more than 2 tomatoSlices, if num of tomatoSlices is even, if we have enough tomatoes for cheese
+    if(tomatoSlices < 2 || tomatoSlices % 2 || cheeseSlices * 2 > tomatoSlices){
+        return []
+    }
+    
+    // while we have cheeseSlices, distribute smallBurgers
+    for(let i = 0; cheeseSlices > 0; i++){
+        cheeseSlices -= 1
+        tomatoSlices -= 2
+        smallBurgers += 1
+    }
+    
+    // while we have tomatoSlices, distribute 2 additional slices to each burger (making sure to substract from smallBurgers and add to jumboBurgers).
+    for(let i = 0; tomatoSlices > 0; i++){
+        // condition to make sure we have enough smallBurgers to change into largeBurgers (or else it goes into negative)
+        if(smallBurgers === 0){
+            return []
+        }
+        tomatoSlices -= 2
+        smallBurgers -= 1
+        jumboBurgers += 1
+    }
+    
+    // push our burgers to the answers array
+    totalBurgers.push(jumboBurgers, smallBurgers)
+    
+    return totalBurgers
 };
