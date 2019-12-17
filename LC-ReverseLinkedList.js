ec0d79ec0d9a3d7245adf7288c955b8c37fcddcd
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-12-17 13:07:40 -0800

    Update LC-ReverseLinkedList.js

--- LC-ReverseLinkedList.js
+++ LC-ReverseLinkedList.js
@@ -6,5 +6,16 @@
 // Output: 5->4->3->2->1->NULL
 
 var reverseList = function(head) {
-
+    let current = head
+    let next = head
+    let prev = null
+    
+    while(current !== null){
+        next = next.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+    
+    return prev
 }
