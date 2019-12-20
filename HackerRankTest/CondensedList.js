293ef45145a715f21ba32dc27dc04ed57222900c
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-12-20 14:46:42 -0800

    Create CondensedList.js

---
+++ HackerRankTest/CondensedList.js
@@ -0,0 +1,32 @@
+// Given a list of integers, remove any nodes that have values that have previously occurred in the list and
+// return a reference to the head of the list.
+
+// Function Description
+// Complete the function condense in the editor below. The function must return a reference to a
+// LinkedListNode, the first node of a list that contains only the unique value nodes from the original list, in
+// order.
+// condense has the following parameter(s):
+//  head: the head of a singly-linked list of integers, a LinkedListNode
+ 
+// Note:. A LinkedListNode has two attributes: data, an integer, and next, a reference to the next item in the
+// list or the language equivalent of null at the tail.
+
+/*
+ * Complete the 'condense' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST head as parameter.
+ */
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ * int data;
+ * SinglyLinkedListNode next;
+ * }
+ *
+ */
+ 
+function condense(head) {
+
+}
