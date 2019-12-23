84845220de1adb72dcde899673bbd252b1580347
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2019-12-23 15:18:46 -0800

    Update LC-FindKPairsWithSmallestSums.js

--- LC-FindKPairsWithSmallestSums.js
+++ LC-FindKPairsWithSmallestSums.js
@@ -26,5 +26,21 @@
  */
  
 var kSmallestPairs = function(nums1, nums2, k) {
-
+    // sort the arrays, make sure to take into account 10 (javascript's way of sorting ints)
+    // loop through arrays creating new array holding sum of numbers
+    // sort the array
+    // return the first k pairs
+    
+    let sums = [];
+    
+    for(let i = 0; i < nums1.length; i++){
+        for(let j = 0; j < nums2.length; j++){
+            sums.push([nums1[i], nums2[j]]);
+        }
+    }
+    
+    let sortedSums = sums.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]))
+    
+    let kPairs = sortedSums.splice(0, k)
+    return kPairs
 };
