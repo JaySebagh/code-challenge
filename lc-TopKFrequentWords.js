198be833649b11412e0d84c6fd6f80a0bcc912c6
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-01-27 13:26:55 -0800

    Update lc-TopKFrequentWords.js

--- lc-TopKFrequentWords.js
+++ lc-TopKFrequentWords.js
@@ -10,5 +10,52 @@
 // Note that "i" comes before "love" due to a lower alphabetical order.
 
 let topKFrequent = function(words, k) {
-
+    // loop through the array and add words to an object
+        // if the word already exists, increment by 1
+        // if the word doesn't exist, add it
+    // sort the object into an array, first by frequency, then alphabetically
+    // return k amount
+    
+    // object that will hold our frequency counter
+    let counter = {}
+    // array that will convert our object so we can sort
+    let toArr = []
+    // our final answer
+    let output = []
+    
+    // loop over the words array
+    // if the word is already in the object increment it, if not add it
+    for(let i = 0; i < words.length; i++){
+        words[i] in counter ? counter[words[i]]++ : counter[words[i]] = 1
+    }
+    
+    // convert to array
+    for(let i in counter) {
+        toArr.push([i, counter[i]]);
+    }
+    
+    // sort the array
+    toArr.sort(function(a,b){
+        // sort by frequency
+        if(a[1] > b[1]){
+            return -1;
+        } else if(a[1] < b[1]){
+            return 1;
+        }
+        
+        // sort alphabetically
+        if(a[0] < b[0]){
+            return -1;
+        } else if(a[0] > b[0]) {
+            return 1;
+        }
+    })
+    
+    // loop through the array and return the top k frequencies
+    for(let i = 0; i < k; i++){
+        output.push(toArr[i][0]);
+    }
+    
+    // return the top frequencies
+    return output
 }
