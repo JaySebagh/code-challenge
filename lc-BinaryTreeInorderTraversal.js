11e489ce8ccef341125e1c7ab6543a2a1c59107d
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-01-28 12:57:11 -0800

    Update lc-BinaryTreeInorderTraversal.js

--- lc-BinaryTreeInorderTraversal.js
+++ lc-BinaryTreeInorderTraversal.js
@@ -1,5 +1,25 @@
 // Given a binary tree, return the inorder traversal of its nodes' values.
 
 let inorderTraversal = function(root) {
-
+    // variable to hold inorder values
+    let values = []
+    
+    // function to traverse
+    let traverse = function(node){
+        // check if the node is not null
+        if(node){
+            // call traverse function and pass in left
+            traverse(node.left)
+            // push the value
+            values.push(node.val)
+            // call traverse function and pass in right
+            traverse(node.right)
+        }
+    }
+    
+    // run traverse function and pass in the root
+    traverse(root)
+    
+    // return in order traversal
+    return values
 }
