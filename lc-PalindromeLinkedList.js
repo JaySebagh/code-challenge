608f9bd9bd3f1df3f6992f0a3a438d5db2c451a1
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-04 09:43:49 -0800

    Update lc-PalindromeLinkedList.js

--- lc-PalindromeLinkedList.js
+++ lc-PalindromeLinkedList.js
@@ -1,5 +1,24 @@
 // Given a singly linked list, determine if it is a palindrome.
 
 let isPalindrome = function(head) {
-
+    let values = []
+    let next = head;
+    
+    while(next){
+        values.push(next.val)
+        next = next.next
+    }
+    
+    // if(!head || values.length === 1) return true
+    
+    // let reverseValues = [...values].reverse().join('')
+    // values = values.join('')
+    
+    // return values === reverseValues ? true : false
+    
+    for(let i = 0; i < values.length / 2; i++){
+        if(values[i] != values[values.length - 1 - i]) return false
+    }
+    
+    return true
 }
