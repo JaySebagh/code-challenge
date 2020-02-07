32d44394ffe0a3336b734d29314e580a2fbbc24f
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-07 13:20:49 -0800

    Create lc-MinimumIncrementToMakeArrayUnique.js

---
+++ lc-MinimumIncrementToMakeArrayUnique.js
@@ -0,0 +1,7 @@
+// Given an array of integers A, a move consists of choosing any A[i], and incrementing it by 1.
+
+// Return the least number of moves to make every value in A unique.
+
+let minIncrementForUnique = function(A) {
+
+}
