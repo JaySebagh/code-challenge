441f50d47c89fb3d3a87503851283578c0d7e80b
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-07 12:14:06 -0800

    Create lc-FindNumbersWithEvenNumbersOfDigits.js

---
+++ lc-FindNumbersWithEvenNumbersOfDigits.js
@@ -0,0 +1,17 @@
+// Given an array nums of integers, return how many of them contain an even number of digits. 
+
+// Input: [12,345,2,6,7896]
+
+// Output: 2
+
+// Explanation:
+// 12 contains 2 digits (even).
+// 345 contains 3 digits (odd).
+// 2 contains 1 digit (odd). 
+// 6 contains 1 digit (odd). 
+// 7896 contains 4 digits (even). 
+// Therefore only 12 and 7896 contain an even number of digits.
+
+let findNumbers = function(nums) {
+
+}
