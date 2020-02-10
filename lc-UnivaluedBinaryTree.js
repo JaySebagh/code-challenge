d22b5470f861b607618553fd50006d795720832d
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-10 09:18:54 -0800

    Update lc-UnivaluedBinaryTree.js

--- lc-UnivaluedBinaryTree.js
+++ lc-UnivaluedBinaryTree.js
@@ -3,5 +3,49 @@
 // Return true if and only if the given tree is univalued.
 
 let isUnivalTree = function(root) {
-
+    // traverse tree using bfs/dfs
+    // store root value
+    // if node value is different from root value return false
+    // else true
+    
+    let univalued = true
+    
+    let traverse = function(node){
+        if(node){
+            traverse(node.left)
+            traverse(node.right)
+            if(node.val !== root.val){
+                univalued = false
+            }
+        }
+    }
+    
+    traverse(root)
+    
+    return univalued
 }
+
+
+_______________________________________________________________________________________________________
+
+Other people's solutions:
+_______________________________________________________________________________________________________
+
+let isUnivalTree = function(root, val = root.val) {
+    if(!root) return true;
+    
+    return root.val === val && isUnivalTree(root.left, val) && isUnivalTree(root.right, val);
+};
+_______________________________________________________________________________________________________
+
+let isUnivalTree = function(root, value = root.val) {
+  if (!root) {
+    return true;
+  }
+  
+  if (root.val !== value) {
+    return false;
+  }
+  
+  return isUnivalTree(root.left, value) && isUnivalTree(root.right, value);
+};
