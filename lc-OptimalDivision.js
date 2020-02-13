69834ea4bffc680cef03bfe00c8059ed90a72574
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-13 08:49:02 -0800

    Update lc-OptimalDivision.js

--- lc-OptimalDivision.js
+++ lc-OptimalDivision.js
@@ -5,5 +5,25 @@
 // Your expression should NOT contain redundant parenthesis.
 
 let optimalDivision = function(nums) {
+    // return LargestNum/(division of other nums)
+    
+    // test case for 1 number
+    if(nums.length === 1) return `${nums[0]}`
+    // test case for 2 numbers
+    if(nums.length === 2) return `${nums[0]}/${nums[1]}`
+    // variable removes and holds the first number
+    let largest = nums.shift()
+    // variable joins the other numbers together, separated by '/'
+    let otherNums = nums.join('/')
+    // return the largest number divided by the division of the rest
+    return `${largest}/(${otherNums})`
+}
 
+
+Other people's solutions:
+// same thing I did except in 1 line instead of 3.
+let optimalDivision = function(nums) {
+    if (nums.length === 1) return `${nums[0]}`
+    if (nums.length === 2) return `${nums[0]}/${nums[1]}`
+    return `${nums[0]}/(${nums.slice(1).join('/')})`
 }
