38d417e90ee1c27f434bcc57aa1f1dbd722d3251
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-17 09:02:04 -0800

    Create lc-RelativeRanks.js

---
+++ lc-RelativeRanks.js
@@ -0,0 +1,5 @@
+// Given scores of N athletes, find their relative ranks and the people with the top three highest scores, who will be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal".
+
+let findRelativeRanks = function(nums) {
+
+}
