3aa8bfbfb0ede4971c6fcd115aac204608dd2c31
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-19 10:49:29 -0800

    Update lc-MaxConsecutiveOnes.js

--- lc-MaxConsecutiveOnes.js
+++ lc-MaxConsecutiveOnes.js
@@ -1,5 +1,15 @@
 // Given a binary array, find the maximum number of consecutive 1s in this array.
 
 let findMaxConsecutiveOnes = function(nums) {
+    // split the array
+    // return longest one
+    
+    return nums.join('').split('0').sort((a, b) => b.length - a.length)[0].length
+};
 
-}
+________________________________________________________________________________
+Other people's solutions:
+let findMaxConsecutiveOnes = function(nums) {
+    return Math.max(...nums.join('').split('0').map(ones => ones.length));
+};
+________________________________________________________________________________
