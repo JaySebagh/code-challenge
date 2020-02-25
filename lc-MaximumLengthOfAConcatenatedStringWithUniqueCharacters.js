a47f9a0be632945aa6b920e1698be44b14e37a63
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-24 17:19:05 -0800

    Create lc-MaximumLengthOfAConcatenatedStringWithUniqueCharacters.js

---
+++ lc-MaximumLengthOfAConcatenatedStringWithUniqueCharacters.js
@@ -0,0 +1,7 @@
+// Given an array of strings arr. String s is a concatenation of a sub-sequence of arr which have unique characters.
+
+// Return the maximum possible length of s.
+
+let maxLength = function(arr) {
+
+}
