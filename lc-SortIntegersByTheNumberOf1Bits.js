dba5417f602af65eea36425438b220d8ea06a018
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-25 11:39:41 -0800

    Create lc-SortIntegersByTheNumberOf1Bits.js

---
+++ lc-SortIntegersByTheNumberOf1Bits.js
@@ -0,0 +1,8 @@
+// Given an integer array, sort the integers in ascending order by the number of 1's in their binary representation
+// In case of two or more integers have the same number of 1's, sort them in ascending order.
+
+// Return the sorted array.
+
+sortByBits = function(arr) {
+
+}
