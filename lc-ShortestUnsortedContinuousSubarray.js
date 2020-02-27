49bffbb744ede92979c4a734f5445270ec47a7dc
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-26 18:11:53 -0800

    Update lc-ShortestUnsortedContinuousSubarray.js

--- lc-ShortestUnsortedContinuousSubarray.js
+++ lc-ShortestUnsortedContinuousSubarray.js
@@ -4,5 +4,28 @@
 // You need to find the shortest such subarray and output its length.
 
 let findUnsortedSubarray = function(nums) {
-
+    // realized that bubble sort isn't the best method since I would have to iterate multiple times until conditions are met
+    
+    // currently non functional
+    
+    
+    // bubble sort
+    // if right < current swap
+    
+    let startIndex = 0;
+    let endIndex = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > nums[i + 1]){
+            if(startIndex === 0){
+                startIndex = i
+            }
+            endIndex = i + 2
+            let temp = parseInt(nums.slice(i, i + 1).join())
+            nums[i] = nums[i + 1];
+            nums[i + 1] = temp
+        }
+    }
+    
+    console.log(nums, startIndex, endIndex)
 }
