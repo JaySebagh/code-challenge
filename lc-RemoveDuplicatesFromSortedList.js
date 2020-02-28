bf51cdfc9f5c682b0f41288690b9d058427c8622
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-02-28 12:21:59 -0800

    Update lc-RemoveDuplicatesFromSortedList.js

--- lc-RemoveDuplicatesFromSortedList.js
+++ lc-RemoveDuplicatesFromSortedList.js
@@ -1,5 +1,18 @@
 // Given a sorted linked list, delete all duplicates such that each element appear only once.
 
 let deleteDuplicates = function(head) {
-
+    // loop through the linked list
+    // if current val is same as next, change current's next to next's next
+    // else continue looping
+    
+    let curr = head;
+    
+    while(curr){
+        if(curr.next != null && curr.val === curr.next.val){
+            curr.next = curr.next.next
+        } else {
+            curr = curr.next
+        }
+    }
+    return head
 }
