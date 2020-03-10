8e7db4c0986ff93085fa1c76f4880a31621bc91b
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-03-10 15:43:30 -0700

    Update lc-GoatLatin.js

--- lc-GoatLatin.js
+++ lc-GoatLatin.js
@@ -16,5 +16,30 @@
 // Return the final sentence representing the conversion from S to Goat Latin. 
 
 let toGoatLatin = function(S) {
-
+    // split the string and loop over
+    // check if first letter is vowel
+        // if it is append 'ma'
+        // if it isn't, move the first letter to the end and append 'ma'
+    // repeat letter 'a' i + 1 times and append to each word
+    
+    // split string
+    let words = S.split(' ')
+    for(let i = 0; i < words.length; i++){
+        // toLowerCase for case sensitivity
+        let ltr = words[i][0].toLowerCase()
+        // check if vowel
+        if(ltr == "a" || ltr == "e" || ltr == "i" || ltr == "o" || ltr == "u"){
+            // append 'ma'
+            words[i] = words[i] + 'ma'
+        } else {
+            // append first letter and remove it, append 'ma'
+            words[i] = (words[i] + words[i][0] + 'ma').slice(1)
+        }
+        // repeat 'a' i + 1 times
+        const repeatA = 'a'.repeat(i + 1)
+        // append to each word
+        words[i] = words[i] + repeatA
+    }
+    
+    return words.join(' ')
 }
