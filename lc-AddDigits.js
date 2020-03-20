31584d55540501b6f6d5ceeaa79371e0e113cf14
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-03-19 18:07:11 -0700

    Update lc-AddDigits.js

--- lc-AddDigits.js
+++ lc-AddDigits.js
@@ -1,5 +1,19 @@
 // Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
 
 const addDigits = function(num) {
-
+    // if num === single digit, return num
+    if(num.toString(10).length === 1) return num
+    // while num !== single digit, add the digits together and replace num
+    while(num.toString(10).length !== 1){
+        num = Array.from(num.toString()).map(Number).reduce((a, b) => a + b)
+    }
+    return num
 }
+
+___________________________________________
+Other people's submissions:
+
+const addDigits = function(num) {
+    return (num % 9 === 0) ? 9 : num % 9
+}
+___________________________________________
