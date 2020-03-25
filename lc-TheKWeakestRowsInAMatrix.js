d83ea7485c059a8c47175a98fd434710cb5c4069
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-03-25 13:38:35 -0700

    Create lc-TheKWeakestRowsInAMatrix.js

---
+++ lc-TheKWeakestRowsInAMatrix.js
@@ -0,0 +1,10 @@
+// Given a m * n matrix mat of ones (representing soldiers) and zeros (representing civilians),
+// return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest.
+
+// A row i is weaker than row j, if the number of soldiers in row i is less than the number of soldiers in row j,
+// or they have the same number of soldiers but i is less than j. Soldiers are always stand in the frontier of a row, that is,
+// always ones may appear first and then zeros.
+
+const kWeakestRows = function(mat, k) {
+
+}
