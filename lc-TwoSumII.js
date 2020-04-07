9027e5e6c15362692e63dd2439f83f43286af001
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-04-07 16:11:16 -0700

    Update lc-TwoSumII.js

--- lc-TwoSumII.js
+++ lc-TwoSumII.js
@@ -5,5 +5,21 @@
 // where index1 must be less than index2.
 
 const twoSum = function(numbers, target) {
+    for(let i = 0; i < numbers.length; i++){
+        console.log(i)
+        if(numbers.includes(target - numbers[i])){
+            return [i + 1, numbers.indexOf(target - numbers[i], i+1) + 1]
+        }
+    }
+};
 
-}
+________________________________________________________________
+Other People's Solutions:
+________________________________________________________________
+const twoSum = function(numbers, target) {
+    for(let i = 0; i < numbers.length; i++) {
+        let j = numbers.indexOf(target - numbers[i], i + 1);
+        if (j !== -1) return [i + 1, j + 1];
+    }    
+};
+________________________________________________________________
