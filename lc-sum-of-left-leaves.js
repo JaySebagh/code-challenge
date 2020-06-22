1c137a7b925589f841febaa6fbc287480c90138a
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-06-22 10:27:16 -0700

    Update lc-sum-of-left-leaves.js

--- lc-sum-of-left-leaves.js
+++ lc-sum-of-left-leaves.js
@@ -6,20 +6,34 @@ let sumOfLeftLeaves = function(root) {
     // add left node value to the left sum tree value variable
     
     // variable to hold left tree sum value
-    let sumLeftTree = 0;
+//     let sumLeftTree = 0;
     
-    // function to traverse the tree, passing in a node
+//     // function to traverse the tree, passing in a node
+//     let traverse = (node) => {
+//      // if there is a node left, add it's value to sumLeftTree and pass the node to the traverse function
+//      if(node.left){
+//          sumLeftTree += node.left.val
+//          traverse(node.left)
+//      }
+//      // if there is a node right, pass it to the traverse function
+//      if(node.right) traverse(node.right)
+//     }
+    
+//     traverse(root)
+    
+//     return sumLeftTree
+    let sumLeftTree = 0;
     let traverse = (node) => {
-     // if there is a node left, add it's value to sumLeftTree and pass the node to the traverse function
-     if(node.left){
-         sumLeftTree += node.left.val
-         traverse(node.left)
-     }
-     // if there is a node right, pass it to the traverse function
-     if(node.right) traverse(node.right)
+        if(!node) return
+        if(node.left){
+            if(!node.left.left && !node.left.right){
+                sumLeftTree += node.left.val
+            }
+        }
+        traverse(node.left)
+        traverse(node.right)
     }
     
     traverse(root)
-    
     return sumLeftTree
 }
