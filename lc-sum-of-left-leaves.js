f4593c3a6f39f7579ac5d1f517239993c985468c
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-06-22 10:04:15 -0700

    Update lc-sum-of-left-leaves.js

--- lc-sum-of-left-leaves.js
+++ lc-sum-of-left-leaves.js
@@ -1,5 +1,25 @@
 Find the sum of all left leaves in a given binary tree.
 
-sumOfLeftLeaves = function(root) {
-
+let sumOfLeftLeaves = function(root) {
+    // create a variable to hold left tree sum value
+    // traverse the list
+    // add left node value to the left sum tree value variable
+    
+    // variable to hold left tree sum value
+    let sumLeftTree = 0;
+    
+    // function to traverse the tree, passing in a node
+    let traverse = (node) => {
+     // if there is a node left, add it's value to sumLeftTree and pass the node to the traverse function
+     if(node.left){
+         sumLeftTree += node.left.val
+         traverse(node.left)
+     }
+     // if there is a node right, pass it to the traverse function
+     if(node.right) traverse(node.right)
+    }
+    
+    traverse(root)
+    
+    return sumLeftTree
 }
