43b24b331574ebb15880a8fca93f990e1242919a
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-08-10 11:43:16 -0700

    Update lc-number-of-substrings-containing-all-three-characters.js

--- lc-number-of-substrings-containing-all-three-characters.js
+++ lc-number-of-substrings-containing-all-three-characters.js
@@ -2,5 +2,30 @@
 // Return the number of substrings containing at least one occurrence of all these characters a, b and c.
 
 const numberOfSubstrings = function(string) {
-
+    // loop through the string and keep searching till a, b, and c are found
+    // if found, return substring and all other substrings
+    
+    if(!string.includes('a') || !string.includes('b') || !string.includes('c')){
+        return 0
+    }
+    
+    let characters = ['a', 'b', 'c'];
+    const removeFirstOccurance = characters.splice(characters.indexOf(string[0]), 1);
+    let remainingCharacters;
+    let loopEndPosition;
+    for(let i = 0; i < string.length; i++){
+        if(i === string.length - 1){
+            
+        }
+        if(characters.length === 0){
+            remainingCharacters = string.substr(i, string.length)
+            loopEndPosition = i
+            break
+        }
+        if(characters.includes(string[i])){
+            characters.splice(string[i], 1)
+        }
+    }
+    console.log(remainingCharacters)
+    // return occuranceCount
 }
