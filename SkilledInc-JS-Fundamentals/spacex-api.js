2af6914480c89036c542fdd4afb5df697c7dc110
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-08-31 22:54:47 -0700

    Create spacex-api.js

---
+++ SkilledInc-JS-Fundamentals/spacex-api.js
@@ -0,0 +1,3 @@
+// Create a program that will pull data from SpaceX api 'https://api.spacexdata.com/v3/history’ and display wikipedia link for each history event that comes back.
+// Wikipedia link is stored in the links object. Do not print null results.
+// Please implement using regular promises as well as using async/await syntax.
