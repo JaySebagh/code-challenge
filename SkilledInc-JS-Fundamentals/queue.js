e26e05c7f6ec7d2066f9b6eff3feff7c8c5c8e5b
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-09-08 16:15:35 -0700

    Update queue.js

--- SkilledInc-JS-Fundamentals/queue.js
+++ SkilledInc-JS-Fundamentals/queue.js
@@ -1,2 +1,39 @@
 // Implement Queue data structure with the standard enqueue and dequeue methods.
 // What is the complexity of each method?
+
+class Queue {
+  constructor() {
+      this.storage = {};
+      this.size = 0;
+  }
+
+  enqueue(item) {
+    this.storage[this.size] = item;
+    this.size++;
+    return `enqueue ${item}`;
+  }
+
+  dequeue() {
+    let item = this.storage[0];
+    delete this.storage[0];
+    this.size--;
+    return `dequeue ${item}`;
+  }
+
+  peek(){
+    return this.storage[0];
+  }
+
+  display(){
+    return this.storage;
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+
+console.log("initial", myQueue.display())
+console.log(myQueue.enqueue(4), myQueue.display())
+console.log(myQueue.dequeue(), myQueue.display())
