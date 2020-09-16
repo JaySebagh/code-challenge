101a474fa8bf02ab8808217ba5ac56748dae394e
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2020-09-15 18:40:04 -0700

    Update majority.js

--- SkilledInc-JS-Fundamentals/majority.js
+++ SkilledInc-JS-Fundamentals/majority.js
@@ -1,7 +1,19 @@
 Create a function "majority" that accepts an array and a callback. The callback will take a parameter and will return either true or false. majority will iterate through the array and perform the callback on each element until it can be determined if the majority of the return values from the callback are true. If the number of true returns is equal to the number of false returns, majority should return false.
 
 function majority(inputArray, callback) {
+  trueCount = 0;
+  falseCount = 0;
 
+  function checkMajority(num, check){
+    return check(num)
+  }
+
+  for(i in inputArray){
+    const isGreater = checkMajority(inputArray[i], callback)
+    isGreater ? trueCount++ : falseCount++
+  }
+
+  return trueCount > falseCount ? true : false
 }
 
 // testing setup
