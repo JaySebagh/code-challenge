581e52da965dfc9203354d37b6b0b990eddc21d9
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2022-08-05 15:43:52 -0700

    solved

--- leetcode-contains-duplicate.js
+++ leetcode-contains-duplicate.js
@@ -8,5 +8,18 @@
 // if it does return true else false
 
 const containsDuplicate = function(nums) {
-  
+    const numsMap = new Map();
+
+    for (i in nums){
+        if(!numsMap.has(nums[i])){
+            numsMap.set(nums[i], '')
+        } else {
+            return true
+        }
+    }
+    return false
 };
+
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+
+// alternative method: use 'set'
