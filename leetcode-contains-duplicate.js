5a92f6cbd3289bfc804e2269a91d8fdc10e4a963
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2022-08-05 15:43:25 -0700

    Create leetcode-contains-duplicate.js

---
+++ leetcode-contains-duplicate.js
@@ -0,0 +1,12 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// brainstorm/sudo code
+// create a map
+// go through the input array
+// for each integer in the array, add the integer as a key in the map
+// check if the key exists in the map already
+// if it does return true else false
+
+const containsDuplicate = function(nums) {
+  
+};
