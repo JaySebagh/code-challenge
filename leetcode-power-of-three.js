af54f35bc67f7a965829dfb4db798f405c3d59af
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2022-08-14 13:04:21 -0700

    solved

--- leetcode-power-of-three.js
+++ leetcode-power-of-three.js
@@ -1,5 +1,9 @@
 // https://leetcode.com/problems/power-of-three/
 
 const isPowerOfThree = function(num) {
-
+    return Math.log10(num) / Math.log10(3) % 1 === 0
 };
+
+// other solutions
+
+// https://leetcode.com/problems/power-of-three/discuss/77876/**-A-summary-of-all-solutions-(new-method-included-at-15%3A30pm-Jan-8th)
