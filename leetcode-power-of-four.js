7f97ac297e99979c4d66fede0e23e61e143e5380
Jay Sebagh <46274307+JaySebagh@users.noreply.github.com>
2022-08-15 13:36:42 -0700

    solved

--- leetcode-power-of-four.js
+++ leetcode-power-of-four.js
@@ -1,5 +1,13 @@
 // https://leetcode.com/problems/power-of-four/submissions/
 
 const isPowerOfFour = function(num) {
-  
+    return Math.log(num) / Math.log(4) % 1 === 0
 };
+
+console.log(isPowerOfFour(0)) // false
+console.log(isPowerOfFour(1)) // true
+console.log(isPowerOfFour(2)) // false
+console.log(isPowerOfFour(3)) // false
+console.log(isPowerOfFour(4)) // true
+console.log(isPowerOfFour(5)) // false
+console.log(isPowerOfFour(16)) // true
