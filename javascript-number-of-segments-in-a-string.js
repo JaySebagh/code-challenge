0f5b158bdd96d904134c8a45a133e64fb0c3b993
Jay Sebagh <jaysebagh@gmail.com>
2022-08-27 17:54:37 -0700

    solved

--- javascript-number-of-segments-in-a-string.js
+++ javascript-number-of-segments-in-a-string.js
@@ -1,5 +1,39 @@
 // https://leetcode.com/problems/number-of-segments-in-a-string/
 
+// brainstorm/sudo code
+// split the string by whitespace and return the length
+
+// version 1
+// works except for whitespace edgecases such as countSegements("     ")
 const countSegments = function(str) {
-  
-}
+    return str.split(" ").length
+};
+
+// version 2
+const countSegments = function(str) {
+    const arr = str.split(" ");
+    let len = arr.length;
+    for(segment of arr){
+        if(segment === "") len--;
+    };
+    return len;
+};
+
+// version 3
+const countSegments = function(str) {
+    return str.split(" ").filter(segment => segment.trim().length > 0).length;
+};
+
+console.log(countSegments("Hello, my name is John")); // 5
+console.log(countSegments("                ")); // 0
+
+// other solutions
+const countSegments = function(str) {
+    return str.split(' ').filter(Boolean).length;
+};
+
+//
+
+const countSegments = function(str) {
+    return str.split(' ').filter(segment => segment.length > 0).length;
+};
