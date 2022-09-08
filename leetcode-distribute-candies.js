068b8f864a7e392d974234fcc01eff29b56749fb
Jay Sebagh <jaysebagh@gmail.com>
2022-09-08 13:28:39 -0700

    solved

--- leetcode-distribute-candies.js
+++ leetcode-distribute-candies.js
@@ -1,5 +1,22 @@
 // https://leetcode.com/problems/distribute-candies/
 
+// brainstorm/sudo code
+// loop through array
+// add to Set
+// return Set length or half of array length
+
 const distributeCandies = function(candyType) {
-  
+    let candies = new Set();
+    
+    for(candy of candyType) candies.add(candy)
+        
+    return candies.size > candyType.length / 2 ? candyType.length / 2 : candies.size 
 };
+
+console.log(distributeCandies([1,1,2,2,3,3])) // 3
+console.log(distributeCandies([1,1,2,3])) // 2
+console.log(distributeCandies([6,6,6,6])) // 1
+
+// other solutions
+
+const distributeCandies = candies => Math.min(new Set(candies).size, candies.length / 2);
