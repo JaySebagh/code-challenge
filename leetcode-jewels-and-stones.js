05f1cd7ce3f6c57fe90fc4f667d29f24a0a65922
Jay Sebagh <jaysebagh@gmail.com>
2022-09-19 17:41:44 -0700

    solved

--- leetcode-jewels-and-stones.js
+++ leetcode-jewels-and-stones.js
@@ -1,5 +1,23 @@
 // https://leetcode.com/problems/jewels-and-stones/
 
-const numJewelsInStones = function(jewels, stones) {
+// brainstorm/sudocode
+// add stones to Map
+// loop through jewels and return count
 
+const numJewelsInStones = function(jewels, stones) {
+    let count = new Map(), counter = 0;
+    
+    for(let stone of stones) count.set(stone, count.get(stone) + 1 || 1);
+    for(let jewel of jewels) counter += count.get(jewel) || 0
+
+    return counter
+};
+
+console.log(numJewelsInStones("z", "ZZ")) // 0
+
+// other solutions
+
+const numJewelsInStones = (jewels, stones) => {
+    const count = new Set(jewels)
+    return stones.split('').reduce((res, stone) => res + count.has(stone), 0)
 };
