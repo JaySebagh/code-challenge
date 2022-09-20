ad2595d90ac4da1e16fc46c4d486f0e51a342c17
Jay Sebagh <jaysebagh@gmail.com>
2022-09-20 16:16:41 -0700

    solved

--- leetcode-rotate-string.js
+++ leetcode-rotate-string.js
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/rotate-string/
 
-const rotateString = function(str, goal) {
+// brainstorm/sudocode
+// loop through str length
+// remove first letter in str and add it to the end
+// check if str === goal
 
+const rotateString = function(str, goal) {
+    for (let i = 0; i < str.length - 1; i++) {
+        str = (str + str.charAt(0)).slice(1)
+        if (str === goal) return true
+    }
+    return false
+};
+
+console.log(rotateString("apple", "banana")) // false
+console.log(rotateString("apple", "pleap")) // true
+
+// other solutions
+
+const rotateString = function(str, goal) {
+    return (goal + goal).indexOf(str) != -1 && str.length == goal.length
+};
+
+//
+
+const rotateString = function(str, goal) {
+    for(let i = 0; i < str.length; i++){
+        if(str.slice(i) + str.slice(0, i) === goal) return true;
+    }
+    return goal.length ? false : true;
 };
