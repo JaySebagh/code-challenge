15218f6c211021c83c4444d6eb31cf513285e671
Jay Sebagh <jaysebagh@gmail.com>
2022-10-27 22:24:00 +0900

    solved

--- leetcode-remove-all-adjacent-duplicates-in-string.js
+++ leetcode-remove-all-adjacent-duplicates-in-string.js
@@ -1,5 +1,40 @@
 // https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 
-const removeDuplicates = function(str) {
+// brainstorm/sudo code
+// loop through the string
+// check if curr matches next
+// if it does remove them
+// restart the loop
 
+const removeDuplicates = function(str) {
+    str = str.split("");
+    
+    for(let i = 0; i < str.length - 1; i++){
+        if(str[i] === str[i + 1]){
+            str.splice(i, 2);
+            i -= 2;
+        };
+    };
+    
+    return str.join("")
+};
+
+console.log(removeDuplicates("abbaca")) // "ca"
+
+
+// other solutions
+
+const removeDuplicates = str => {
+    const stack = [];
+    
+    for(const char of str) stack[stack.length - 1] === char ? stack.pop() : stack.push(char);
+    
+    return stack.join('');
+};
+
+//
+
+const removeDuplicates = str => {
+    const remove = str.replace(/(.)\1/g, '');
+    return remove.length === str.length ? str : removeDuplicates(remove);
 };
