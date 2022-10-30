80de4b5e6c6a442b7e421b831f241c3bf49e65dd
Jay Sebagh <jaysebagh@gmail.com>
2022-10-30 09:34:29 -0700

    solved

--- leetcode-defanging-an-ip-address.js
+++ leetcode-defanging-an-ip-address.js
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/defanging-an-ip-address/
 
-const defangIPaddr = function(address) {
+// brainstorm/sudo code
+// replace using regex
 
+const defangIPaddr = function(address) {
+    return address.replace(/[.]/g, "[.]")
+};
+
+console.log(defangIPaddr("1.1.1.1")) // "1[.]1[.]1[.]1"
+
+
+// other solutions
+
+const defangIPaddr = function(address) {
+    let result = "";
+    
+    for(let i = 0; i < address.length; i++) {
+        if(address[i] === ".")
+            result += "[.]";
+        else
+            result += address[i];
+    }
+    
+    return result;    
 };
