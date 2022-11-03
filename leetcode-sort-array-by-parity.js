aaa5ffa7ed49af0b819fc4cb08bd50d9e16222d9
Jay Sebagh <jaysebagh@gmail.com>
2022-11-02 19:27:38 -0700

    solved

--- leetcode-sort-array-by-parity.js
+++ leetcode-sort-array-by-parity.js
@@ -1,5 +1,28 @@
 // https://leetcode.com/problems/sort-array-by-parity/
 
-const sortArrayByParity = function(nums) {
+// brainstorm/sudo code
+// loop through the nums
+// check if even or odd
+// push number to corresponding array
+// merge even and odd arrays with even in front
 
+// using 2 arrays and spreading them
+const sortArrayByParity = function(nums) {
+    let even = [], odd = [];
+
+    for(let num of nums) num % 2 === 0 ? even.push(num) : odd.push(num);
+
+    return [...even, ...odd]
 }
+
+console.log(sortArrayByParity([3,1,2,4])) // [4,2,3,1] [2,4,1,3] [4,2,1,3] 
+
+
+// other solutions
+
+const sortArrayByParity = function(nums) {
+    const temp = [];
+    nums.forEach(v => v % 2 === 0 ? temp.unshift(v) : temp.push(v));
+    
+    return temp;
+};
