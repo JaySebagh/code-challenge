8b5a74ab8d85b0f0063996a90aa19f5d4971664e
Jay Sebagh <jaysebagh@gmail.com>
2022-11-05 10:30:54 -0700

    solved

--- leetcode-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ leetcode-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -1,5 +1,17 @@
 // https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 
-const subtractProductAndSum = function(num) {
+// brainstorm/sudo code
+// split num
+// reduce multiplying each value
+// reduce adding each value
+// subtract the two
 
+const subtractProductAndSum = function(num) {
+    const digits = Array.from(String(num), Number);
+    const multiply = digits.reduce((a, b) => a * b);
+    const add = digits.reduce((a, b) => a + b);
+    return multiply - add
 };
+
+console.log(subtractProductAndSum(234)) // 15
+console.log(subtractProductAndSum(4421)) // 21
